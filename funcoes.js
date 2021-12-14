28347c00cc1ad1589ee19c13e04ab641a0619dc2
Anderson Ramos <andersonramos07@live.com>
2021-12-13 23:23:15 -0300

    correções e atualizações de 13DEC2021

--- funcoes.js
+++ funcoes.js
@@ -6,13 +6,17 @@ const resultados = async (loteria) =>{
     await page.goto(`http://loterias.caixa.gov.br/wps/portal/loterias/landing/${loteria}`);
     page.waitForNavigation();
 
+    if(loteria){
+        const consurso = page.evaluate(() => { return Array.from(document.querySelectorAll(".ng-binding"), elem => { if (elem.innerText){ console.log(elem.innerText)}})})
+    }
+
     switch (loteria){
         case "lotofacil":
         case "lotomania":
-            return await page.evaluate((queryS) => { return Array.from(document.querySelectorAll(".lista-dezenas li"), el => el.innerText) });
+            return await page.evaluate(() => { return Array.from(document.querySelectorAll(".lista-dezenas li"), el => el.innerText) });
             break;
         case "duplasena":
-            return await page.evaluate((queryS) => { return Array.from(document.querySelectorAll(".dupla-sena li"), el => el.innerText) });
+            return await page.evaluate(() => { return Array.from(document.querySelectorAll(".dupla-sena li"), el => el.innerText) });
             break;
         case "diadesorte":
         case "megasena":        
@@ -30,4 +34,14 @@ const resultados = async (loteria) =>{
     await browser.close();
 };
 
-module.exports = {resultados}
+const concursos = async (loteria) =>{
+    const browser = await puppet.launch();
+    const page = await browser.newPage();
+    await page.goto(`http://loterias.caixa.gov.br/wps/portal/loterias/landing/${loteria}`);
+    page.waitForNavigation();
+
+    return await page.evaluate(() => { return Array.from(document.querySelectorAll(".ng-binding"), elem => elem.innerText) });
+};
+
+
+module.exports = {resultados, concursos}
--- public/javascript/scripts.js
+++
@@ -1,7 +0,0 @@
-const submitResultados = () =>{
-    var select = document.querySelector('#opcaoLoteria');
-
-    var valor = select.options[select.selectedIndex].value;
-
-    //return location.href="/resultados"+"/"+valor;
-};
